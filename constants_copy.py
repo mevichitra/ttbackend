efc6cbcc2b5cf639a7f92248229f5dbbaf773762
mevichitra <mevichitra@gmail.com>
2024-10-18 12:58:45 +0530

    add a static copy of constants

---
+++ constants_copy.py
@@ -0,0 +1,8 @@
+payment_url = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay" # can be changed into prod url
+base_url = "https://api-preprod.phonepe.com/apis/pg-sandbox" # can be changed into prod url
+webhook_url = "<<your webhook url>>"
+payment_endpoint = "/pg/v1/pay"
+status_endpoint = "/pg/v1/status"
+merchant_id = "<<your merchant id>>"
+salt_key = "<<your sal key>>"
+salt_index = "<<your index key>>"
